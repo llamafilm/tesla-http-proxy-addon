c1eb8abef354d5d7ac53a8fcf195146a9e15cee3
Elliott Balsley <3991046+llamafilm@users.noreply.github.com>
2024-05-27 17:16:34 -0700

    fix: curl retry with exponential backoff

--- tesla_http_proxy/rootfs/app/run.sh
+++ tesla_http_proxy/rootfs/app/run.sh
@@ -74,35 +74,12 @@ else
   generate_keypair
 fi
 
-# verify domain $DOMAIN has an associated IP address, if not loop and retry
-bashio::log.info "Testing $DOMAIN for an associated IP address..."
-while :; do
-  if ! host $DOMAIN; then
-    bashio::log.error "$DOMAIN has no associated IP address, add a record in your DNS config."
-    bashio::log.error "Sleeping 2 minutes before retry."
-    sleep 2m
-  else
-    bashio::log.info "Found an IP address for $DOMAIN"
-    break
-  fi
-done
-
 # verify public key is accessible with valid TLS cert
 bashio::log.info "Testing public key..."
-CURL_OUT=$(curl -sfD - "https://$DOMAIN/.well-known/appspecific/com.tesla.3p.public-key.pem" || true)
-echo "$CURL_OUT"
-# last HTTP status code (in case of a redirect)
-HTTP_STATUS_CODE=$(echo "$CURL_OUT"|awk '/^HTTP/{print $2}'|tail -1)
-while :; do
-  if [ "$HTTP_STATUS_CODE" -eq 200 ]; then
-    # All good
-    bashio::log.info "The public key is accessible."
-    break
-  else
-    bashio::log.error "HTTP status code $HTTP_STATUS_CODE; Sleeping 2 minutes before retry."
-    sleep 2m
-  fi
-done
+if ! curl -fD - --no-progress-meter --max-time 5 --connect-timeout 2 --retry 14 --retry-all-errors "https://$DOMAIN/.well-known/appspecific/com.tesla.3p.public-key.pem"; then
+  bashio::log.fatal "Fix public key before proceeding."
+  exit 1
+fi
 
 if [ -z "$CLIENT_ID" ]; then
   bashio::log.notice "Request application access with Tesla, then fill in credentials and restart addon."
