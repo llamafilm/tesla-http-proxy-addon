27e137ee2fa3cb70264eda94a22e4e0364d9b4b8
Elliott Balsley <3991046+llamafilm@users.noreply.github.com>
2024-05-27 19:22:49 -0700

    fix: standalone mode

--- DEVELOPERS.md
+++ DEVELOPERS.md
@@ -19,3 +19,19 @@ curl --cacert /share/tesla/selfsigned.pem \
     --header "Authorization: Bearer $TESLA_AUTH_TOKEN" \
     "https://c03d64a7-tesla-http-proxy/api/1/vehicles"
 ```
+
+### Standalone Usage
+
+While this addon is meant to run in HAOS, it may be useful to run as a standalone Docker container, for CI pipelines or debugging the Tesla integration in a dev container.  This is a work-in-progress script to do that.
+
+Th `start_proxy.sh` script will start 2 Docker containers, one for Nginx and one for the proxy.  It mimics some HAOS concepts including folder structure and bashio so you can use the same Docker image as the addon.  You may need to modify according to your environemnt.
+
+- Forward https://DOMAIN:443 to localhost:4430
+- Start Docker
+- Clone this repo
+- Navigate to the `standalone` folder
+- Copy TLS cert and key to `ssl/fullchain.pem` and `ssl/privkey.pem`
+- Set environment variables in `secrets.env`
+- Run `start_proxy.sh`
+- Start OAuth at http://localhost:8099 and it will callback to https://DOMAIN/callback
+- After getting the token, edit `addons.self.options.config.cache` to change true to false and then restart
--- standalone/start_proxy.sh
+++ standalone/start_proxy.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# see README.md for instructions
+# see DEVELOPERS.md for instructions
+
+#########################################################
+# Define these variables in a new file called secrets.env
+ARCH=aarch64 # options: aarch64, amd64, armhf, armv7, i386
+DEBUG=true # must be lowercase
+REGION="North America, Asia-Pacific" # options defined in const.py 
+DOMAIN=
+CLIENT_ID=
+CLIENT_SECRET=
+#########################################################
 
 set -e
-
+cd "$(dirname "$0")"
 source secrets.env
 mkdir -p  nginx
 # start nginx container
-openssl dhparam -out nginx/dhparams.pem 2048
+openssl dhparam -dsaparam -out nginx/dhparams.pem 2048
 sed "s/__DOMAIN__/${DOMAIN}/g; s/__PROXYHOST__/tesla_http_proxy/g" ../tesla_http_proxy/rootfs/app/nginx_tesla.conf > nginx/nginx_tesla.conf
 
-echo "Making sure we have a clean start if needed ..." 
+echo "Making sure we have a clean start if needed ..."
 docker rm -f nginx
 docker rm -f tesla_http_proxy
 docker network rm tesla
@@ -32,7 +42,7 @@ docker run --rm --name nginx -d -p 4430:443 -e DOMAIN="$DOMAIN" --network tesla 
 # docker build --build-arg BUILD_FROM=ghcr.io/home-assistant/${ARCH}-homeassistant-base tesla-http-proxy-addon/tesla_http_proxy
 
 # start proxy container
-
+# fake token used for Tesla integration CI tests
 docker run --rm --name tesla_http_proxy -p 8099:8099 -p 443:443 --network tesla \
     -v ./share:/share \
     -v ./bashio:/tmp/.bashio:ro \
@@ -41,4 +51,5 @@ docker run --rm --name tesla_http_proxy -p 8099:8099 -p 443:443 --network tesla 
     -e CLIENT_SECRET="${CLIENT_SECRET}" \
     -e REGION="${REGION}" \
     -e SUPERVISOR_TOKEN="fake-token" \
-    ghcr.io/llamafilm/tesla_http_proxy_${ARCH}:1.3.6
+    -e DEBUG="${DEBUG}" \
+    ghcr.io/llamafilm/tesla_http_proxy_"${ARCH}":2.2.6
